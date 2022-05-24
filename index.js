6a1d32fd65ed37f49a4a888bffb29f248c3c719d
yousefelmahdy <62335520+yousefelmahdy@users.noreply.github.com>
2022-05-24 02:24:25 +0200

    MongoDB connection

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 
-app.listen(5000, ()=>{
-    console.log("server is running")
-})
+mongoose.connect('mongodb://localhost/Ecommerce')
+        .then(()=>console.log('connected successfully'))
+        .catch(err=>console.log('could not connect....'));
+
+const port = process.env.Port || 3000;
+app.listen(port, () => console.log(`lisening on port ${port}`));
+
